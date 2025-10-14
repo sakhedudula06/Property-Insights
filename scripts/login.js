dfa4e3d8cffe4699c56cbf24216a23ac7b4dfef3
sakhedudula06 <kamvelihledudula@gmail.com>
2025-10-14 10:50:51 +0000

    Enhance UI and functionality: update canvas dimensions, add tenant data display, and create new password recovery page

--- scripts/login.js
+++ scripts/login.js
@@ -1,26 +1,22 @@
 // A JavaScript object that holds your user data, acting as a local database.
 const usersData = {
   "users": [
-    { "username": "sakhekile", "password": "12345" },
-    { "username": "admin", "password": "admin@123" }
+    { "username": "sakhekile", "password": "12345", "email": "kamvelihledudula@gmail.com"},
+    { "username": "admin", "password": "admin@123", "email": "sakhekile.dudula@mrisoftware.com" }
   ]
 };
 
-// Function to handle the login logic
 function login() {
-  // Get the form and input values
+
   const usernameInput = document.getElementById('username').value;
   const passwordInput = document.getElementById('password').value;
 
-  // Find a user that matches both the username and password
   const user = usersData.users.find(
     (u) => u.username === usernameInput && u.password === passwordInput
   );
 
-  // Check if a user was found
   if (user) {
     alert("Login successful!");
-    // Redirect to the home page or dashboard
     window.location.href = "home.html";
   } else {
     alert("Invalid username or password");
@@ -32,3 +28,9 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
   event.preventDefault(); // Prevents the default form submission behavior (page reload)
   login(); // Call the login function
 });
+
+function handleCostKeydown(event){
+    if(event.key === 'Enter'){
+    login();
+  };
+};
