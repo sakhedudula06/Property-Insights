08889852a402242eef86293b8a141e0b48e72a06
sakhedudula06 <kamvelihledudula@gmail.com>
2026-03-11 18:35:55 +0200

    feat: implement tenants controller and router with Supabase integration

---
+++ backend/src/controllers/tenants.controller.js
@@ -0,0 +1,35 @@
+import supabase from "../config/supabase-js.js";
+
+export async function getAllTenants(_, res) {
+  try {
+    const { data, error } = await supabase.from("tenants").select();
+
+    if (error) {
+      console.error("Supabase error:", error);
+      return res.status(502).json({ error: error.message });
+    }
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Unexpected failure:", err);
+    return res.status(500).json({ error: "Server error" });
+  }
+}
+
+export async function insertTenants(req, res) {
+  try {
+    const { data, error } = await supabase
+      .from("tenants")
+      .insert(req.body)
+      .select();
+
+    if (error) {
+      console.error("Supabase error:", error);
+      return res.status(502).json({ error: error.message });
+    }
+
+    res.status(200).json(data);
+  } catch (error) {}
+}
+
+
