d1089f5e38daebd57f1daf70f8fd4d1c5a15818e
Sakhekile-Dudula <sakhekile.dudula@mrisoftware.com>
2026-03-17 19:36:22 +0200

    feat: initialize frontend with React, Vite, and Tailwind CSS
    - Added main application structure with routing using React Router.
    - Created initial pages: HomePage, Index, NewUser, ForgetPass, Payments, Reports, Settings, and Tenants.
    - Integrated toast notifications using react-hot-toast.
    - Included SVG icons for various functionalities.
    - Set up Tailwind CSS for styling.
    - Configured Vite for development and build processes.
    - Added global CSS file for Tailwind directives.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Route, Routes } from 'react-router'
+import ForgetPass from './pages/ForgetPass.jsx'
+import HomePage from './pages/HomePage.jsx'
+import Index from './pages/Index.jsx'
+import NewUser from './pages/NewUser.jsx'
+import Payments from './pages/Payments.jsx'
+import Reports from './pages/Reports.jsx'
+import Settings from './pages/Settings.jsx'
+import Tenants from './pages/Tenants.jsx'
+import toast from 'react-hot-toast'
+
+
+const App = () => {
+  return (
+    <div>
+      <button onClick={() => toast.success("Success")} className='text-red-500 p-4 bg-pink-300'>Click me</button>
+      <Routes>
+        <Route path='/dashboard' element ={<HomePage />} />
+        <Route path='/forgetpass' element ={<ForgetPass />} />
+        <Route path='/' element ={<Index />} />
+        <Route path='/newuser' element={<NewUser />} />
+        <Route path='/payments' element={<Payments />} />
+        <Route path='/reports' element={<Reports />} />
+        <Route path='/settings' element={<Settings />} />
+        <Route path='/tenants' element={<Tenants />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App
