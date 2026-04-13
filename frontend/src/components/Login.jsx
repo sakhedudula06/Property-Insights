0f64edd58bab5603ecb10c5ec2ab5da23cba9eef
sakhedudula06 <kamvelihledudula@gmail.com>
2026-04-13 11:31:13 +0200

    feat: add login component and update index page layout
    - Added a new Login component for user authentication.
    - Updated the Index page to include the Login component and improved layout with a grid design.
    - Included new assets: MRI logo, transparent image, and a PNG for the rising trend line.
    - Added nodemon as a development dependency for better server management.

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='bg-white p-14 shadow-md rounded-xl'>
+      <div className='text-center'>
+        <p className='font-bold text-[#183450]'>Welcome Back</p>
+        <p>Sign in to your Property Insights account</p>
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Login
--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import Login from '../components/Login.jsx'
+import { House } from 'lucide-react';
 
 const Index = () => {
   return (
-    <div className='min-h-screen bg-[rgb(244,244,244)]'>
-      
+    <div className='min-h-screen bg-[rgb(250,250,250)] grid grid-cols-2'>
+      <div className='bg-[rgba(235,245,250,1)] p-16'>
+        <div className='mb-20'>
+          <img src='/src/assets/MRI_Logo_transparent.svg' className='w-48' />
+        </div>
+        <div className='mb-5'>
+          <div className='bg-sky-200 w-60 p-3 text-[#0d59b0] flex gap-5 rounded-lg mb-8'>
+            <House />
+            <p className='font-bold'>PROPERTY INSIGHTS</p>
+          </div>
+
+          <div className='mb-8'>
+            <p className='font-bold text-[#183450] text-7xl'>Smarter Data.</p>
+            <p className='font-bold text-[#183450] text-7xl'>Better Decisions.</p>
+          </div>
+
+          <div className='text-gray-500 text-3xl'>
+            <p>Sign in to access powerful insights and</p>
+            <p>streamline your property management.</p>
+          </div>
+        </div>
+        <div>
+          <img className='h-[290px]' src='/src/assets/transparent.png' />
+        </div>
+
+      </div>
+
+      <div className='p-16'>
+        <Login />
+      </div>
     </div>
   )
 }
