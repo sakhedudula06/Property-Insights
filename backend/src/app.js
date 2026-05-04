df69ecb350218ada30b023eeb90c445f083193e4
Sakhekile-Dudula <sakhekile.dudula@mrisoftware.com>
2026-05-04 14:39:05 +0200

    fix: correct indentation in production routing for serving static files

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,8 +25,8 @@ app.use("/api/v1/tenants", tenantsRouter);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")))
   app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-})
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  })
 }
 
 
